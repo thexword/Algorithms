9df9b369843a36c7b7171dd053ae0928bb75f792
thexword <thexwordchs@gmail.com>
2022-02-13 15:55:39 +0800

    add methods for TrieST

--- app/src/main/java/com/example/algorithms/algorithms/strings/TrieST.java
+++ app/src/main/java/com/example/algorithms/algorithms/strings/TrieST.java
@@ -1,5 +1,7 @@
 package com.example.algorithms.algorithms.strings;
 
+import com.example.algorithms.datastructures.Queue;
+
 public class TrieST<Value> {
     private static int R = 256; //radix
     private Node root; // root of trie
@@ -73,5 +75,110 @@ public class TrieST<Value> {
         }
 
         return cnt;
+    }
+
+    public Iterable<String> keys() {
+        return keysWithPrefix("");
+    }
+
+    public Iterable<String> keysWithPrefix(String pre) {
+        Queue<String> queue = new Queue<>();
+        collect(get(root, pre, 0), pre, queue);
+        return queue;
+    }
+
+    private void collect(Node node, String pre, Queue<String> queue) {
+        if (null == node) {
+            return;
+        }
+
+        if (node.val != null) {
+            queue.enqueue(pre);
+        }
+
+        for (char c = 0; c < R; ++c) {
+            collect(node.next[c], pre + c, queue);
+        }
+    }
+
+    public Iterable<String> keysThatMatch(String pat) {
+        Queue<String> queue = new Queue<>();
+        collect(root, "", pat, queue);
+        return queue;
+    }
+
+    private void collect(Node node, String pre, String pat, Queue<String> queue) {
+        if (null == node) {
+            return;
+        }
+
+        int d = pre.length();
+
+        if (d == pat.length()) {
+            if (node.val != null) {
+                queue.enqueue(pre);
+            }
+
+            return;
+        }
+
+        char next = pat.charAt(d);
+
+        for (char c = 0; c < R; c++) {
+            if (next == '.' || next == c) {
+                collect(node.next[c], pre + c, pat, queue);
+            }
+        }
+    }
+
+    public String longestPrefixOf(String s) {
+        int length = search(root, s, 0, 0);
+        return s.substring(0, length);
+    }
+
+    private int search(Node x, String s, int d, int length) {
+        if (x == null) {
+            return length;
+        }
+
+        if (x.val != null) {
+            length = d;
+        }
+
+        if (d == s.length()) {
+            return length;
+        }
+
+        char c = s.charAt(d);
+        return search(x.next[c], s, d+1, length);
+    }
+
+    public void delete(String key) {
+        root = delete(root, key, 0);
+    }
+
+    private Node delete(Node x, String key, int d) {
+        if (x == null) {
+            return null;
+        }
+
+        if (d < key.length()) {
+            char c = key.charAt(d);
+            x.next[c] = delete(x.next[c], key, d+1);
+        } else {
+            x.val = null;
+        }
+
+        if (x.val != null) {
+            return x;
+        }
+
+        for (char c = 0; c < R; c++) {
+            if (x.next[c] != null) {
+                return x;
+            }
+        }
+
+        return null;
     }
 }
